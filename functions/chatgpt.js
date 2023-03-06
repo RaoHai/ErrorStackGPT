a9f9493423e8c54f695b164194860b155c30bb3f
陆沉 <surgesoft@gmail.com>
2023-03-06 22:05:38 +0800

    feat: add chatgpt

---
+++ functions/chatgpt.js
@@ -0,0 +1,11 @@
+import { ChatGPTAPI } from 'chatgpt'
+
+const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+export default async function handler(req, res) {
+    const { text = 'hello world' } = req.query;
+    const res = await api.sendMessage(text)
+    res.status(200).json({ result: res.text });
+}
